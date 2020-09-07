763e455409010577bd8641d02c0f2ccf25226d0a
alexandre-roa <alexandre.z.roa@gmail.com>
2020-09-07 18:40:31 -0300

    add routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,9 +8,9 @@ import PostTransaction from '../pages/Post';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={PostTransaction} />
+    <Route path="/" exact component={Dashboard} />
     <Route path="/import" component={Import} />
-    <Route path="/post" component={Dashboard} />
+    <Route path="/post" component={PostTransaction} />
   </Switch>
 );
 
